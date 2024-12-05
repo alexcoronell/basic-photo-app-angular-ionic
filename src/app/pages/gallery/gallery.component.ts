9b3aa71958c4dcd1a148f5e0af5018bf50608f1b
Alex Coronell <alexcoronell@gmail.com>
2024-12-05 18:27:09 -0500

    Photo service partial completed and tested

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon, IonImg, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+
+import { PhotosService } from 'app/services/photos.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon, IonImg, IonGrid, IonRow, IonCol]
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
+  photosService = inject(PhotosService)
 
-  constructor() { }
+  photos: string[] = []
 
-  ngOnInit() { }
+  constructor(){
+    this.photos = this.photosService.photos;
+  }
+
+  async takePhoto() {
+    await this.photosService.addNewPhoto()
+  }
 
 }
