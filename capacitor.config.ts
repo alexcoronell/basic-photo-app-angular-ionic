88e54f8d9ef23cd6003e851b8f466b099aaa21e5
Alex Coronell <alexcoronell@gmail.com>
2024-12-05 17:31:33 -0500

    Capacitor and capacitor camera installed

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.alexcoronell.app',
+  appName: 'photos-app',
+  webDir: 'dist/photos-app'
+};
+
+export default config;
--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonFab, IonFabButton, IonIcon]
 })
 export class GalleryComponent implements OnInit {
 
