59566fa507593b1c5d4eac25f0e11845ff464f6e
Alex Coronell <alexcoronell@gmail.com>
2024-12-05 14:39:19 -0500

    Detail button and back button added

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonBackButton,
+  ],
 })
-export class DetailComponent  implements OnInit {
-
-  constructor() { }
+export class DetailComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [RouterLink, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  ngOnInit() {}
 }
