a1f22b067fe5a820086d3ac78f4d31a7f1c15b82
Alex Coronell <alexcoronell@gmail.com>
2024-12-05 14:22:19 -0500

    Routes completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'detail',
+    loadComponent: () =>
+      import('./pages/detail/detail.component').then((c) => c.DetailComponent),
+  },
+  {
+    path: 'gallery',
+    loadComponent: () =>
+      import('./pages/gallery/gallery.component').then(
+        (c) => c.GalleryComponent
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
